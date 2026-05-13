535eb1123ae90ae047df2ee4a1b93e98b40f360f
Rymicka <116494816+Rymicka@users.noreply.github.com>
2026-05-13 19:19:51 +0200

    Refactor get-loader.ps1 for improved encoding handling

--- get-loader.ps1
+++ get-loader.ps1
@@ -1,2 +1,6 @@
-[Console]::OutputEncoding = [Text.Encoding]::UTF8
-iex ([Text.Encoding]::UTF8.GetString((Invoke-WebRequest "https://blinkymc.xyz/install/install.ps1").Content))
+[Console]::OutputEncoding = [System.Text.UTF8Encoding]::new()
+
+$response = Invoke-WebRequest "https://blinkymc.xyz/install/install.ps1"
+$script = [System.Text.Encoding]::UTF8.GetString($response.RawContentStream.ToArray())
+
+iex $script
